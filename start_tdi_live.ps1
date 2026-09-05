9a30d692d2f9fd396518fd1d54b25a885c5b237c
Patrice Pion <saintrigaudedition@gmail.com>
2026-09-05 18:28:35 +0200

    Add USDCAD to TDI live watchlist

--- start_tdi_live.ps1
+++ start_tdi_live.ps1
@@ -11,6 +11,7 @@ python -m scripts.run_tdi_mt5 `
     AUDUSD `
     USDJPY `
     USOUSD `
+    USDCAD `
     --monitor `
     --interval 60 `
     --dashboard-dir "C:\Users\user\AppData\Roaming\MetaQuotes\Terminal\D0E8209F77C8CF37AD8BF550E51FF075\MQL5\Files"

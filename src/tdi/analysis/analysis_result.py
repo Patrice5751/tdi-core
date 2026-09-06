12d7303501b4f7fc602a0d970b6ba712509a5a05
Patrice Pion <saintrigaudedition@gmail.com>
2026-09-06 14:39:26 +0200

    Remove duplicate AnalysisResult definition

--- src/tdi/analysis/analysis_result.py
+++ src/tdi/analysis/analysis_result.py
@@ -1,19 +1,6 @@
 from dataclasses import dataclass
 
 from tdi.analysis.momentum_analysis import MomentumAnalysis
-from tdi.analysis.structure_analysis import StructureAnalysis
-from tdi.analysis.trend_analysis import TrendAnalysis
-from tdi.analysis.multi_timeframe_trend import MultiTimeframeTrend
-
-
-@dataclass(frozen=True)
-class AnalysisResult:
-    trend: TrendAnalysis
-    momentum: MomentumAnalysis
-    structure: StructureAnalysis
-from dataclasses import dataclass
-
-from tdi.analysis.momentum_analysis import MomentumAnalysis
 from tdi.analysis.multi_timeframe_trend import MultiTimeframeTrend
 from tdi.analysis.structure_analysis import StructureAnalysis
 from tdi.analysis.trend_analysis import TrendAnalysis
